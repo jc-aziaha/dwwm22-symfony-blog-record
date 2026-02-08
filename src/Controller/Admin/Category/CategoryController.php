16553401fca10cf728ccbc33b239ecfed2485a7e
Vega <aziaha.formations@gmail.com>
2026-02-08 16:50:57 +0100

    feat: L'administrateur peut maintenant ajouter de nouvelles catégories à la liste

---
+++ src/Controller/Admin/Category/CategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\Admin\Category;
+
+use App\Entity\Category;
+use App\Form\Admin\CategoryFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin')]
+final class CategoryController extends AbstractController
+{
+    #[Route('/category/list', name: 'app_admin_category_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('pages/admin/category/index.html.twig');
+    }
+
+    #[Route('/category/create', name: 'app_admin_category_create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $category = new Category();
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category->setCreatedAt(new \DateTimeImmutable());
+            $category->setUpdatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La catégorie a été ajoutée à la liste.');
+
+            return $this->redirectToRoute('app_admin_category_index');
+        }
+
+        return $this->render('pages/admin/category/create.html.twig', [
+            'categoryForm' => $form->createView(),
+        ]);
+    }
+}
