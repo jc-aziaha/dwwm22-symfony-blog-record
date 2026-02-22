b7d05842c7721f0d3448c9d638c989e3e4098675
Vega <aziaha.formations@gmail.com>
2026-02-22 19:49:23 +0100

    feat: L'administrateur peut maintenant modifier son mot de passe

--- src/Controller/Admin/Profile/ProfileController.php
+++ src/Controller/Admin/Profile/ProfileController.php
@@ -3,11 +3,13 @@
 namespace App\Controller\Admin\Profile;
 
 use App\Entity\User;
+use App\Form\Admin\EditPasswordProfileFormType;
 use App\Form\Admin\ProfileFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin')]
@@ -15,6 +17,7 @@ final class ProfileController extends AbstractController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $hasher,
     ) {
     }
 
@@ -48,6 +51,38 @@ final class ProfileController extends AbstractController
 
         return $this->render('pages/admin/profile/edit.html.twig', [
             'profileForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/profile/password-edit', name: 'app_admin_profile_password_edit', methods: ['GET', 'POST'])]
+    public function editPassword(Request $request): Response
+    {
+        $form = $this->createForm(EditPasswordProfileFormType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var User
+             */
+            $admin = $this->getUser();
+
+            $formData = $form->getData();
+
+            $passwordHashed = $this->hasher->hashPassword($admin, $formData['password']);
+
+            $admin->setPassword($passwordHashed);
+            $admin->setUpdatedAt(new \DateTimeImmutable());
+
+            $this->entityManager->persist($admin);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Le mot de passe a bien été modifié');
+
+            return $this->redirectToRoute('app_admin_profile_index');
+        }
+
+        return $this->render('pages/admin/profile/edit_password.html.twig', [
+            'passwordProfileForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/Admin/EditPasswordProfileFormType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form\Admin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class EditPasswordProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('currentPassword', PasswordType::class, [
+                'constraints' => [
+                    new UserPassword([
+                        'message' => 'Le mot de passe actuel est invalide.',
+                    ]),
+                ],
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Le nouveau mot de passe est obligatoire.',
+                        ]),
+                        new Length([
+                            'min' => 12,
+                            'max' => 255,
+                            'minMessage' => 'Le mot de passe doit contenir au minimum {{ limit }} caractères.',
+                            'maxMessage' => 'Le mot de passe doit contenir au maximum {{ limit }} caractères.',
+                        ]),
+                        new Regex([
+                            'pattern' => '/^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ỳ])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ỳ0-9]).{11,255}$/',
+                            'match' => true,
+                            'message' => 'Le mot de passe doit contentir au moins une lettre miniscule et majuscule, un chiffre et un caractère spécial.',
+                        ]),
+                    ],
+                ],
+                'invalid_message' => 'Le mot de passe doit être identique à sa confirmation.',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
