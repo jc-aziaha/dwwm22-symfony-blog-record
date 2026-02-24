0c5d0d9dc0f51a8f240c2af458f1aa94e1078e6f
Vega <aziaha.formations@gmail.com>
2026-02-24 16:57:25 +0100

    feat: Les visiteurs et les utilisateurs peuvent contactez l'administrateur via la page de contact

--- src/Controller/Visitor/Contact/ContactController.php
+++ src/Controller/Visitor/Contact/ContactController.php
@@ -2,15 +2,68 @@
 
 namespace App\Controller\Visitor\Contact;
 
+use App\Entity\Contact;
+use App\Entity\User;
+use App\Form\ContactFormType;
+use App\Repository\SettingRepository;
+use App\Service\SendEmailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_visitor_contact_index', methods: ['GET'])]
-    public function index(): Response
+    public function __construct(
+        private readonly SettingRepository $settingRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly SendEmailService $sendEmailService,
+    ) {
+    }
+
+    #[Route('/contact', name: 'app_visitor_contact_index', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
-        return $this->render('pages/visitor/contact/index.html.twig');
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User */
+            $user = $this->getUser();
+
+            if (null != $user) {
+                $contact->setUser($user);
+            }
+
+            $contact->setCreatedAt(new \DateTimeImmutable());
+
+            $this->entityManager->persist($contact);
+            $this->entityManager->flush();
+
+            $this->sendEmailService->sendEmail([
+                'sender_email' => 'medecine-du-monde@gmail.com',
+                'sender_full_name' => 'Jean Dupont',
+                'recipient_email' => 'medecine-du-monde@gmail.com',
+                'subject' => 'Un nouveau message reçu sur votre blog',
+                'html_template' => 'emails/contact_form_email.html.twig',
+                'context' => [
+                    'contact' => $contact,
+                ],
+            ]);
+
+            $this->addFlash('success', 'Votre message a bien été envoyée. Je vous recontacterai dans les plus brefs délais.');
+
+            return $this->redirectToRoute('app_visitor_contact_index');
+        }
+
+        $setting = $this->settingRepository->findAll()[0];
+
+        return $this->render('pages/visitor/contact/index.html.twig', [
+            'setting' => $setting,
+            'contactForm' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Service/SendEmailService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+
+class SendEmailService
+{
+    public function __construct(
+        private readonly MailerInterface $mailer,
+    ) {
+    }
+
+
+    /**
+     * Permet d'envoyer un email.
+     *
+     * @param array<string, mixed> $data
+     * @return void
+     */
+    public function sendEmail(array $data = []): void
+    {
+        $email = (new TemplatedEmail())
+            ->from(new Address($data['sender_email'], $data['sender_full_name']))
+            ->to($data['recipient_email'])
+            ->subject($data['subject'])
+            ->htmlTemplate('emails/contact_form_email.html.twig')
+            ->context($data['context'])
+        ;
+
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            throw $e;
+        }
+    }
+}
