8bdbf2ea875fdf5e7780598cf90a436c0113d767
Vega <aziaha.formations@gmail.com>
2026-02-25 19:16:32 +0100

    feat: La liste des articles est affichée sur la page du blog et le système de pagination mis en place

--- config/reference.php
+++ config/reference.php
@@ -1011,7 +1011,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         enabled?: bool|Param, // Default: true
  *     },
  *     string?: bool|array{
- *         enabled?: bool|Param, // Default: false
+ *         enabled?: bool|Param, // Default: true
  *     },
  *     commonmark?: array{
  *         renderer?: array{ // Array of options for rendering HTML.
@@ -1709,6 +1709,28 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         post_processors?: array<string, array<string, mixed>>,
  *     },
  * }
+ * @psalm-type KnpPaginatorConfig = array{
+ *     default_options?: array{
+ *         sort_field_name?: scalar|null|Param, // Default: "sort"
+ *         sort_direction_name?: scalar|null|Param, // Default: "direction"
+ *         filter_field_name?: scalar|null|Param, // Default: "filterField"
+ *         filter_value_name?: scalar|null|Param, // Default: "filterValue"
+ *         page_name?: scalar|null|Param, // Default: "page"
+ *         distinct?: bool|Param, // Default: true
+ *         page_out_of_range?: scalar|null|Param, // Default: "ignore"
+ *         default_limit?: scalar|null|Param, // Default: 10
+ *     },
+ *     template?: array{
+ *         pagination?: scalar|null|Param, // Default: "@KnpPaginator/Pagination/sliding.html.twig"
+ *         rel_links?: scalar|null|Param, // Default: "@KnpPaginator/Pagination/rel_links.html.twig"
+ *         filtration?: scalar|null|Param, // Default: "@KnpPaginator/Pagination/filtration.html.twig"
+ *         sortable?: scalar|null|Param, // Default: "@KnpPaginator/Pagination/sortable_link.html.twig"
+ *     },
+ *     page_range?: scalar|null|Param, // Default: 5
+ *     page_limit?: scalar|null|Param, // Default: null
+ *     convert_exception?: bool|Param, // Default: false
+ *     remove_first_page_param?: bool|Param, // Default: false
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1726,6 +1748,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *     vich_uploader?: VichUploaderConfig,
  *     liip_imagine?: LiipImagineConfig,
+ *     knp_paginator?: KnpPaginatorConfig,
  *     "when@dev"?: array{
  *         imports?: ImportsConfig,
  *         parameters?: ParametersConfig,
@@ -1746,6 +1769,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *         vich_uploader?: VichUploaderConfig,
  *         liip_imagine?: LiipImagineConfig,
+ *         knp_paginator?: KnpPaginatorConfig,
  *     },
  *     "when@prod"?: array{
  *         imports?: ImportsConfig,
@@ -1764,6 +1788,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *         vich_uploader?: VichUploaderConfig,
  *         liip_imagine?: LiipImagineConfig,
+ *         knp_paginator?: KnpPaginatorConfig,
  *     },
  *     "when@test"?: array{
  *         imports?: ImportsConfig,
@@ -1783,6 +1808,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         stof_doctrine_extensions?: StofDoctrineExtensionsConfig,
  *         vich_uploader?: VichUploaderConfig,
  *         liip_imagine?: LiipImagineConfig,
+ *         knp_paginator?: KnpPaginatorConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
--- src/Controller/Visitor/Blog/BlogController.php
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -2,15 +2,42 @@
 
 namespace App\Controller\Visitor\Blog;
 
+use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
+use App\Repository\TagRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
+    public function __construct(
+        private readonly PostRepository $postRepository,
+        private readonly CategoryRepository $categoryRepository,
+        private readonly TagRepository $tagRepository,
+        private readonly PaginatorInterface $paginator,
+    ) {
+    }
+
     #[Route('/blog', name: 'app_visitor_blog_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('pages/visitor/blog/index.html.twig');
+        $categories = $this->categoryRepository->findAll();
+        $tags = $this->tagRepository->findAll();
+        $query = $this->postRepository->findBy(['isPublished' => true]);
+
+        $posts = $this->paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /* page number */
+            10 /* limit per page */
+        );
+
+        return $this->render('pages/visitor/blog/index.html.twig', [
+            'categories' => $categories,
+            'tags' => $tags,
+            'posts' => $posts,
+        ]);
     }
 }
