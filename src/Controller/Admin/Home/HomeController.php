9a81699039631d9ff565cce82ae90858a7ca40e2
Vega <aziaha.formations@gmail.com>
2026-03-09 11:06:52 +0100

    add: L'adminstrateur peut maintenant consulter son tableau de bord

--- src/Controller/Admin/Home/HomeController.php
+++ src/Controller/Admin/Home/HomeController.php
@@ -2,6 +2,13 @@
 
 namespace App\Controller\Admin\Home;
 
+use App\Repository\CategoryRepository;
+use App\Repository\CommentRepository;
+use App\Repository\ContactRepository;
+use App\Repository\LikeRepository;
+use App\Repository\PostRepository;
+use App\Repository\TagRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +16,28 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/admin')]
 final class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly CategoryRepository $categoryRepository,
+        private readonly PostRepository $postRepository,
+        private readonly TagRepository $tagRepository,
+        private readonly CommentRepository $commentRepository,
+        private readonly UserRepository $userRepository,
+        private readonly ContactRepository $contactRepository,
+        private readonly LikeRepository $likeRepository,
+    ) {
+    }
+
     #[Route('/home', name: 'app_admin_home', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/admin/home/index.html.twig');
+        return $this->render('pages/admin/home/index.html.twig', [
+            'categories_counted' => $this->categoryRepository->count(),
+            'posts_counted' => $this->postRepository->count(),
+            'tags_counted' => $this->tagRepository->count(),
+            'comments_counted' => $this->commentRepository->count(),
+            'users_counted' => $this->userRepository->count(),
+            'contacts_counted' => $this->contactRepository->count(),
+            'likes_counted' => $this->likeRepository->count(),
+        ]);
     }
 }
