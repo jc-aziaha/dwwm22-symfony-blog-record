538fb7e3f1096c1edef4929bac0df909f119a25c
Vega <aziaha.formations@gmail.com>
2026-03-14 17:23:25 +0100

    security: Création de la commande adaptée pour la création du super administrateur

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'Impossible de créer un copte avec cet email')]
+#[UniqueEntity(fields: ['email'], message: 'Impossible de créer un compte avec cet email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
